39008c81da59c32502673d7ee23d5a8e920ec0df
Oleksii Ustenko <alexeyu@gmail.com>
2026-08-02 11:28:37 +0200

    chore: downgrade to Node20, sharpen dependecies

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
+  target: "node20",
   dts: true,
   clean: true,
   outExtension({ format }) {
